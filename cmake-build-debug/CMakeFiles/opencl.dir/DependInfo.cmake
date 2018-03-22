5a39a33b6b3276cb4f62d3377f4f75066419ca72
Petrus Kambala <kmbpet001@myuct.ac.za>
2018-03-22 22:17:13 +0200

    Added clFinish method calls to wait for each queue execution

---
+++ cmake-build-debug/CMakeFiles/opencl.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/petrus/CLionProjects/opencl/Tools/OpenCL_Wrapper.c" "/home/petrus/CLionProjects/opencl/cmake-build-debug/CMakeFiles/opencl.dir/Tools/OpenCL_Wrapper.c.o"
+  "/home/petrus/CLionProjects/opencl/Tools/Timer.c" "/home/petrus/CLionProjects/opencl/cmake-build-debug/CMakeFiles/opencl.dir/Tools/Timer.c.o"
+  "/home/petrus/CLionProjects/opencl/main.c" "/home/petrus/CLionProjects/opencl/cmake-build-debug/CMakeFiles/opencl.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/opencl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencl.dir/main.c.o"
+  "CMakeFiles/opencl.dir/Tools/Timer.c.o"
+  "CMakeFiles/opencl.dir/Tools/OpenCL_Wrapper.c.o"
+  "opencl.pdb"
+  "opencl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/opencl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
